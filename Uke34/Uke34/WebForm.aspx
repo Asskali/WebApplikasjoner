7c472e872028d822f2fcedb48fef22ef3663111c
Adam Asskali <s929539@stud.hioa.no>
2016-08-24 13:40:51 +0200

    Ukesoppgaver for uke 34 i Webapplikasjoner

---
+++ Uke34/Uke34/WebForm.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="Uke34.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <asp:Label ID="Label1" runat="server" Text="Kjønn"></asp:Label>
+        <br />
+            <asp:RadioButton ID="RadioButton1" runat="server" text="Mann" GroupName="Kjønn"/>
+            <asp:RadioButton ID="RadioButton2" runat="server" text="Kvinne" GroupName="Kjønn" Checked="true"/>
+        <br/>
+            <asp:Label ID="Label2" runat="server" Text="Alder:"></asp:Label>
+        <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+            <asp:Label ID="Label3" runat="server" Text="Høyde:"></asp:Label>
+        <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+            <asp:Button ID="Button1" runat="server" Text="Fullført" OnClick="Button1_Click" />
+        <br />
+            <asp:Label ID="Label4" runat="server" Text=""></asp:Label>
+        <br />
+        
+    </div>
+    </form>
+    
+</body>
+</html>
---
+++ Uke34/Uke34/WebForm.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Uke34
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if ((RadioButton1.Checked || RadioButton2.Checked) && TextBox1.Text != "" && TextBox2.Text != "")
+            {
+                int alder = Convert.ToInt32(TextBox1.Text);
+                int høyde = Convert.ToInt32(TextBox2.Text);
+                if (RadioButton1.Checked)
+                {
+                    if (alder >= 20 && alder <= 25)
+                        Label4.Text = "Akseptert";
+                    else
+                        Label4.Text = "Ikke akseptert";
+                }
+                else
+                {
+                    if (høyde < 180 && alder == 23)
+                        Label4.Text = "Akseptert";
+                    else
+                        Label4.Text = "Ikke akseptert;";
+                }
+            }
+            else
+                Label4.Text = "Du må fylle inn alle feltene";
+        }
+    }
+}
